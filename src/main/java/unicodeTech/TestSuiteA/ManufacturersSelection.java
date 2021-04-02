d921d1e2c34756358c572e54fee9686f93c8b1a7
AkashM-addweb <52701258+AkashM-addweb@users.noreply.github.com>
2021-04-02 22:03:48 +0530

    Day 2 Changes

--- src/main/java/unicodeTech/TestSuiteA/ManufacturersSelection.java
+++ src/main/java/unicodeTech/TestSuiteA/ManufacturersSelection.java
@@ -1,11 +1,29 @@
 package unicodeTech.TestSuiteA;
 
+import java.util.List;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
 public class ManufacturersSelection extends ParentTestSuiteA{
 
 	
 	public void testManufacturersSelection() {
 		
-	
-	
+		driver.get(siteData.getProperty("url"));
+		
+		WebElement manuDD =isElementPresent("dd_manufactureSelection_name");
+		List<WebElement> manuValues= manuDD.findElements(By.tagName("option"));
+		
+		for(int count=1 ; count<manuValues.size();count++ ) {
+			
+			manuValues.get(count).click();
+			logs.info("Title: "+driver.getTitle() +"    URL:"+driver.getCurrentUrl());
+			driver.navigate().back();
+						
+			//We need to add the below lines again because it is a virtual pointer and the list gets reset everytime the page is refreshed
+			manuDD =isElementPresent("dd_manufactureSelection_name");
+			manuValues= manuDD.findElements(By.tagName("option"));
+		}
+		
 	}
 }
