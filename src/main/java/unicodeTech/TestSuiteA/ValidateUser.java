c7022fd8b93dcea0682aa88f4cd573fc860a5e95
AkashM-addweb <52701258+AkashM-addweb@users.noreply.github.com>
2021-04-02 22:04:31 +0530

    Day 2 changes

--- src/main/java/unicodeTech/TestSuiteA/ValidateUser.java
+++ src/main/java/unicodeTech/TestSuiteA/ValidateUser.java
@@ -1,10 +1,32 @@
 package unicodeTech.TestSuiteA;
 
+import org.openqa.selenium.By;
+
+import unicodeTech.Utility.MyMethods;
+
 public class ValidateUser extends ParentTestSuiteA{
 	
 	public void testValidateUser() {
 		
+		driver.get(siteData.getProperty("url"));
 		
+		MyMethods.signIN("demo@unicodetechnologies.in", "unicode");
+		
+		try {
+			
+		Boolean checkLogin = driver.findElement(By.linkText("Log Off")).isDisplayed();
+		
+		if(checkLogin) {
+			logs.info("user is already logged in");
+			System.out.println("Already logged in");
+			MyMethods.signOUT();
+			
+			}
+	
+		}
+		
+		catch(Exception e) {
+			logs.info("Invalid Username or Password");
+		}
 	}
-
 }
