493ce6326adc5edb8d6325672d9edea31fe12a0b
AkashM-addweb <52701258+AkashM-addweb@users.noreply.github.com>
2021-04-27 18:52:33 +0530

    Update ValidateUser.java

--- src/main/java/unicodeTech/TestSuiteA/ValidateUser.java
+++ src/main/java/unicodeTech/TestSuiteA/ValidateUser.java
@@ -1,32 +1,54 @@
 package unicodeTech.TestSuiteA;
 
 import org.openqa.selenium.By;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
 import unicodeTech.Utility.MyMethods;
 
 public class ValidateUser extends ParentTestSuiteA{
 	
-	public void testValidateUser() {
+	@BeforeTest
+	public void checkTestCase() {
+		
+//		MyMethods.checkTestSuiteExecution(file, sheetName, testSuiteID)
+		boolean output= MyMethods.checkTestCaseExecution(tsa, "TestCases", this.getClass().getSimpleName());
+		logs.info("checkBeforeTest()");
+
+		if(output) {
+			logs.info("Execution of the test case  " + this.getClass().getSimpleName()+ "  is set to Yes");
+		}else {
+			logs.info("Execution of the test case" + this.getClass().getSimpleName()+ "is set to No");
+		}	
+	}
+	
+	@Test(dataProvider="getTestDataFromXLS")
+	public void testValidateUser(String email, String pwd) {
 		
 		driver.get(siteData.getProperty("url"));
+		logs.info("URL Loaded");
+		logs.info("Login with "+email+" and "+pwd);
+		MyMethods.signIN(email,pwd);	
 		
-		MyMethods.signIN("demo@unicodetechnologies.in", "unicode");
-		
-		try {
-			
-		Boolean checkLogin = driver.findElement(By.linkText("Log Off")).isDisplayed();
+		try {	
+		Boolean checkLogin = driver.findElement(By.linkText("Log Off")).isDisplayed();	
 		
 		if(checkLogin) {
 			logs.info("user is already logged in");
 			System.out.println("Already logged in");
-			MyMethods.signOUT();
+			MyMethods.signOUT();	
 			
-			}
-	
-		}
-		
+			}	
+		}		
 		catch(Exception e) {
 			logs.info("Invalid Username or Password");
-		}
+		}	
+	}
+	
+	@DataProvider
+	public Object[][] getTestDataFromXLS() {		
+//		MyMethods.getTestData(data, sheetName)
+		return MyMethods.getTestData(tsa, "ValidateUser");		
 	}
 }
